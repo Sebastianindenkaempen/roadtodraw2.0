b313d7941294eb2655cdbe48a39adcdf0a93c153
Sebastianindenkaempen <Sebastian.indenkaempen@gmail.com>
2026-03-22 11:54:15 +0100

    add inital dataloader class

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+class DataLoader():
+    
+    def extract_data():
+        raise NotImplementedError
+    
+    def transform_data():
+        raise NotImplementedError
+    
+    def load_data():
+        raise NotImplementedError
